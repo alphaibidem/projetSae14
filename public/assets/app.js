ff703d7ca94998e87e265c2bea8f76d72e8bb93c
alphaibidem <120552506+alphaibidem@users.noreply.github.com>
2023-01-20 16:26:21 +0100

    Initial commit

---
+++ public/assets/app.js
@@ -0,0 +1,19 @@
+document.querySelector('.submit-btn').addEventListener('click', function (event) {
+    event.preventDefault(); // empêche la soumission du formulaire
+
+    let emptyFields = false;
+
+    // vérifie si tous les champs sont remplis
+    document.querySelectorAll('.input-field').forEach(function (input) {
+        if (input.value === '') {
+            emptyFields = true;
+        }
+    });
+
+    if (emptyFields) {
+        alert('Tous les champs doivent être remplis');
+    } else {
+        // tous les champs sont remplis, on peut permettre le téléchargement
+        document.querySelector('.submit-btn a').href = 'cv/CV-emploi.pdf';
+    }
+});
